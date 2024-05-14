965cd3cbad84200fb1005cba25452ccc46284cab
Daniel <dani415003@gmail.com>
2024-05-14 13:10:11 +0200

    Commit lista clientes

---
+++ OPENALEX_Mongo.py
@@ -0,0 +1,201 @@
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import os, logging
+
+log_file = os.path.join(os.path.dirname(__file__), 'logs', 'logs_openAlex.log')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename=log_file,
+                    filemode='a')
+
+
+class MongoDB:
+
+    def __init__(self, mongo_uri, db_name):
+        self.mongo_uri = mongo_uri
+        self.db_name = db_name
+
+    def insertar(self, listaTrabajos, numeroTrabajosInsertados):
+
+        if not listaTrabajos:  # Verificar si la lista de documentos está vacía
+            print("No hay documentos para guardar.")
+            return
+        try:
+            client = MongoClient(self.mongo_uri)
+            db = client[self.db_name]
+            collection = db["prueba"]
+
+            collection.insert_many(listaTrabajos)
+            numeroTrabajosInsertados[0] += len(listaTrabajos)
+            listaTrabajos.clear()
+
+            client.close()
+
+        except Exception as e:
+            print("Error")
+
+    def borrartodo(self, nombre_coleccion):
+        client = MongoClient(self.mongo_uri)
+        db = client[self.db_name]
+        collection = db[nombre_coleccion]
+
+        collection.delete_many({})
+        logging.info("Datos borrados correctamente")
+
+    def guardar_fechadescarga(self, id, numero_docs):
+        fecha_actual = datetime.now()
+
+        client = MongoClient(self.mongo_uri)
+        db = client[self.db_name]
+        coleccion = db['fecha_descarga']
+
+        coleccion.insert_one({'fecha': fecha_actual,
+                              'id': id,
+                              'Documentos guardados': numero_docs})
+
+    def obtener_orcid_autor(self):
+        client = MongoClient(self.mongo_uri)
+        db = client[self.db_name]
+        coleccion = db['configuraciones']
+
+        # Obtener el documento de configuraciones
+        documento_conf = coleccion.find_one({})
+
+        autores = {}
+        contador = 0
+
+        for autor in documento_conf.get("autores", []):
+            ids = {"ORCID": '', "SCP": ''}  # Reiniciar el diccionario de identificadores para cada autor
+            for identificador in autor.get("identificadores", []):
+                if identificador.get("tipo") == "ORCID":
+                    ids["ORCID"] = identificador.get("_id")
+                else:
+                    ids["SCP"] = identificador.get("_id")
+            autores[contador] = ids  # Agregar los identificadores al diccionario de autores
+            contador += 1
+
+        return autores
+
+    def isRepetido(self, diccionarioTrabajo, trabajosActualizados):
+        cliente = MongoClient(self.mongo_uri)
+        db = cliente[self.db_name]
+        coleccion = db["prueba"]
+
+        documentos = coleccion.find({"documento.id": diccionarioTrabajo['id']})
+
+        for trabajo in documentos:
+            print(self.compararRepetidosFecha(trabajo, diccionarioTrabajo))
+            if not self.compararRepetidosFecha(trabajo, diccionarioTrabajo):
+
+                nuevaVersion = trabajo["version"] + 1
+                fechaModificacion = datetime.now()
+                coleccion.update_one({"_id": trabajo["_id"]}, {
+                    "$set": {"documento": diccionarioTrabajo, "fechaModi": fechaModificacion, "version": nuevaVersion}})
+                logging.info(f'Trabajo actualizado con id: {trabajo["documento"]["id"]}')
+                trabajosActualizados[0] += 1
+                return True
+            else:
+                logging.info('Ya existe un trabajo identico')
+                cliente.close()
+                return True
+        cliente.close()
+
+        return False
+
+    # Compara un objeto de la coleccion y un diccionario de un trabajo, con el mismo id, si el campo "updated_date" es distinto devuelve true y si no false
+    def compararRepetidosFecha(self, trabajoColeccion, diccionarioTrabajo):
+        if 'updated_date' in trabajoColeccion["documento"] and 'updated_date' in diccionarioTrabajo:
+            return trabajoColeccion["documento"]['updated_date'] != diccionarioTrabajo['updated_date']
+
+    def comprobar_orcid(self, orcid):
+        cliente = MongoClient(self.mongo_uri)
+        db = cliente[self.db_name]
+        coleccion = db["fecha_descarga"]
+
+        documento = coleccion.find_one({"id": orcid})
+
+        if documento:
+            return True
+        else:
+            return False
+
+    def obtener_fecha(self, orcid):
+        cliente = MongoClient(self.mongo_uri)
+        db = cliente[self.db_name]
+        coleccion = db["fecha_descarga"]
+
+        documento = coleccion.find_one({"id": orcid})
+        if documento:
+            return documento["fecha"]
+
+    def obtener_ids_clientes(self):
+        clienteMongo = MongoClient(self.mongo_uri)
+        db = clienteMongo[self.db_name]
+        coleccion = db['configuraciones']
+        listaId = []
+
+        listaIdClientesFechasOrdenadas = self.eliminar_repetidos_listaClientes(self.idClientes_porFecha())
+        listaIdClientes = [doc['clienteId'] for doc in coleccion.find()]
+
+        for clienteId in listaIdClientes:
+            if clienteId not in listaIdClientesFechasOrdenadas:
+                cliente = coleccion.find_one({"clienteId": clienteId})
+                if cliente["enabled"] is True:
+                    listaId.append(cliente)
+
+        for idCliente in listaIdClientesFechasOrdenadas:
+            cliente = coleccion.find_one({"clienteId": idCliente})
+            if cliente["enabled"] is True and self.comprobarFechaCliente(cliente):
+                listaId.append(cliente)
+
+        clienteMongo.close()
+
+        return listaId
+
+    def comprobarFechaCliente(self, cliente):
+        clienteMongo = MongoClient(self.mongo_uri)
+        bd = clienteMongo[self.db_name]
+        coleccionFechas = bd["fecha_descarga"]
+        fechaCliente = coleccionFechas.find_one({"clienteId": cliente["clienteId"]})
+        if (datetime.now() - fechaCliente["fecha"]) > timedelta(days=cliente["periodicidad"]):
+            return True
+
+    def eliminar_repetidos_listaClientes(self, lista):
+        # Creamos un diccionario para almacenar el ID y su posición más baja
+        id_posiciones = {}
+        for i, id in enumerate(lista):
+            if id not in id_posiciones:
+                # Si el ID no está en el diccionario, lo agregamos con su posición
+                id_posiciones[id] = i
+            else:
+                # Si el ID ya está en el diccionario, actualizamos su posición si es menor
+                id_posiciones[id] = min(id_posiciones[id], i)
+
+        # Creamos una lista única usando las posiciones más bajas de cada ID
+        lista_unica = [id for id, pos in sorted(id_posiciones.items(), key=lambda x: x[1])]
+        return lista_unica
+
+    def idClientes_porFecha(self):
+        clienteMongo = MongoClient(self.mongo_uri)
+        db = clienteMongo[self.db_name]
+        coleccionFecha = db["fecha_descarga"]
+
+        resultados = coleccionFecha.find().sort("fecha", 1)
+
+        ids_ordenados = [doc['clienteId'] for doc in resultados]
+
+        clienteMongo.close()
+
+        return ids_ordenados
+
+    def obtener_configuarion_cliente(self, idCliente):
+        cliente = MongoClient(self.mongo_uri)
+        db = cliente[self.db_name]
+        coleccion = db['configuraciones']
+
+        documento = coleccion.find_one({"clienteId": idCliente})
+
+        afiliaciones = documento["affiliations"]
+        autores = documento["autores"]
+
+        return afiliaciones, autores
---
+++ OpenAlex_documentos.py
@@ -0,0 +1,184 @@
+import json, os, logging, requests, math
+from datetime import datetime
+from OPENALEX_Mongo import MongoDB
+
+log_file = os.path.join(os.path.dirname(__file__), 'logs', 'logs_openAlex.log')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename=log_file,
+                    filemode='a')
+
+
+
+class OpenALex:
+
+    def __init__(self):
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+            mongo_uri = config['DEFAULT']["Url_bd"]
+            db_name = config['DEFAULT']["Nombre_bd"]
+        self.mongo = MongoDB(mongo_uri, db_name)
+        self.cicloInserciones = 1000
+        self.listaTrabajos = []
+        self.trabajosActualizados = 0
+        self.numeroTrabajosInsertados = 0
+
+
+    def descargarTodo(self):
+
+        clientes_ids = self.mongo.obtener_ids_clientes()
+        for id in clientes_ids:
+            self.descargarCliente(id)
+
+    def descargarCliente(self, idCliente):
+        resultado = self.mongo.obtener_configuarion_cliente(idCliente)
+
+        afiliaciones, autores = resultado
+
+        for id in afiliaciones:
+            self.descarga_por_institucion(id["affiliationId"])
+
+        self.descarga_por_autores(autores)
+
+    def descarga_por_institucion(self, idInstitucion):
+        url = "https://api.openalex.org/works?filter=institutions.id:" + idInstitucion + "&page={}"
+        self.buscar_docs(url, idInstitucion)
+
+    def descarga_por_autores(self, autores):
+        for autor in autores:
+            orcid = None
+            for identificador in autor.get('identificadores', []):
+                if identificador.get('tipo') == "ORCID":
+                    orcid = identificador.get('_id')
+                    print(orcid)
+                    break
+                elif identificador.get('tipo') == "SCP":
+                    scopus_id = identificador.get('_id')
+                    orcid = self.buscar_orcid_con_scopus(scopus_id)
+                    print(orcid)
+                    break
+
+            if orcid:
+                url = "https://api.openalex.org/works?filter=author.orcid:" + orcid + "&page={}"
+                self.buscar_docs(url, orcid)
+
+    def buscar_orcid_con_scopus(self, idScopus):
+        url = f'https://api.openalex.org/authors?filter=scopus:{idScopus}'
+        data = requests.get(url).json()
+        for autor in data['results']:
+            if autor is None:
+                logging.error(f"No se encontró ORCID para el codigo Scopus:{idScopus}")
+                return None
+            else:
+                return autor['orcid']
+
+
+    def buscar_docs(self, urlpagina, id):
+        totalTrabajosProcesados = 0
+
+        numeroTotalPaginas = self.numero_Total_Paginas(urlpagina.format(1))
+
+        for numeroPagina in range(1, numeroTotalPaginas + 1):
+            try:
+                url = urlpagina.format(numeroPagina)
+                print('\n' + url)
+                data = requests.get(url).json()
+                resultados_pagina = json.loads(json.dumps(data['results']))
+                totalTrabajosProcesados += self.recorrerInsertarTrabajos_porPagina(resultados_pagina)
+            except requests.exceptions.RequestException as e:
+                logging.error(f"Error al realizar la solicitud de la pagina {numeroPagina} : {e}")
+                continue
+        if len(self.listaTrabajos) > 0:
+            self.mongo.insertar(self.listaTrabajos, self.numeroTrabajosInsertados)
+        logging.info(f"Trabajos procesados con id {id} : {totalTrabajosProcesados}")
+        self.mongo.guardar_fechadescarga(id, totalTrabajosProcesados)
+
+
+    def recorrerInsertarTrabajos_porPagina(self, diccionario):
+        contadorTrabajosProcesados = 0
+        self.cambiar_resumen(diccionario)
+
+        for trabajo in diccionario:
+            contadorTrabajosProcesados += 1
+            if not self.mongo.isRepetido(trabajo, self.trabajosActualizados):
+                diccionarioFinal = {"documento": trabajo, "fechaCrea": datetime.now(),
+                                    "fechaModi": datetime.now(), "version": 0}
+                self.listaTrabajos.append(diccionarioFinal)
+                logging.info(f'work añadido a la lista con id: {trabajo["id"]}')
+                if len(self.listaTrabajos) >= self.cicloInserciones:
+                    self.mongo.insertar(self.listaTrabajos, self.numeroTrabajosInsertados)
+        return contadorTrabajosProcesados
+
+
+    def numero_Total_Paginas(self, url):
+        data = requests.get(url).json()
+
+        totalTrabajos = int(data['meta']['count'])
+
+        numeroPaginas = math.ceil(totalTrabajos / 25)
+        return numeroPaginas
+
+    def cambiar_resumen(self, documentos):
+        for documento in documentos:
+            texto = documento.get("abstract_inverted_index")
+            texto_reconstruido = self.reconstruir_abstract(texto)
+            documento["abstract_inverted_index"] = texto_reconstruido
+
+    def reconstruir_abstract(self, resumen):
+        # Crear una lista vacía para almacenar las palabras reconstruidas
+        texto_reconstruido = []
+
+        if resumen is not None:
+            # Iterar sobre las claves y valores del diccionario
+            for palabra, posicion in resumen.items():
+                # Iterar sobre las posiciones de cada palabra
+                for position in posicion:
+                    # Si la posición está dentro del rango actual de la lista reconstruida,
+                    # inserta la palabra en esa posición
+                    if position < len(texto_reconstruido):
+                        texto_reconstruido[position] = palabra
+                    # Si la posición está fuera del rango actual de la lista reconstruida,
+                    # extiende la lista con espacios en blanco hasta la posición correcta
+                    else:
+                        texto_reconstruido.extend([''] * (position - len(texto_reconstruido)))
+                        texto_reconstruido.append(palabra)
+
+        # Unir las palabras reconstruidas en una cadena de texto
+        texto_reconstruido = ' '.join(texto_reconstruido)
+
+        return texto_reconstruido
+
+
+
+if __name__ == "__main__":
+    op = OpenALex()
+    op.descargarTodo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
